654df0c26ec7a429bc8c900b4b559e22daa506a9
steven <806809209@qq.com>
2023-04-24 21:15:00 +0800

    第一次上传

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import torch
-import numpy as np
-
-# 使用torch.cuda.is_available()来确定是否有cuda设备
-x = torch.rand(5, 5, requires_grad=True)
-y = torch.rand(5, 5, requires_grad=True)
-z = x**2+y**3
-z.backward(torch.ones_like(x))
-
-print(x.grad,y.grad)
+print("upload repo")
